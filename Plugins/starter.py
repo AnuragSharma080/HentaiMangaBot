2c679a723470385560670dc83ca9eae9a4c5a084
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-07-21 21:27:24 +0530

    Update starter.py

--- Plugins/starter.py
+++ Plugins/starter.py
@@ -9,7 +9,7 @@ class start():
         await bot.send_message(
             event.chat_id,
             start_text,
-            file='https://telegra.ph/file/1cf0b127f3c888b1d1b21.mp4'
+            file='https://telegra.ph/file/912c648c20b91074b6f9e.mp4'
         )
 
     @bot.on(events.NewMessage(pattern="/help"))
@@ -23,6 +23,6 @@ class start():
     async def event_handler_source(event):
         await bot.send_message(
             event.chat_id,
-            '[Bot Dev](https://t.me/Yeageristbotsdev)\nThis bot was hosted on Heroku'
+            '[Bot Dev](https://t.me/Pain_to_this_world)\nThis bot was hosted on Heroku'
         )
     
