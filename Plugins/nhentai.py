e008edf4b4a972710d6eafd07e742c7883a67994
P4IN SENPAI <86664546+Pain-Senpai@users.noreply.github.com>
2021-12-24 18:24:55 +0530

    Update nhentai.py

--- Plugins/nhentai.py
+++ Plugins/nhentai.py
@@ -11,7 +11,7 @@ class Nhentai():
             await bot.send_message(
                 event.chat_id,
                 'Hentai Manga not found Command must be used like this\n/nh <hentai code\nexample: /nh 339989',
-                file='https://telegra.ph/file/ebe8484d678f49948a2a3.mp4'
+                file='https://telegra.ph/file/824e2857985db147714e4.mp4'
             )
         elif '/nh' in event.raw_text:
             text = event.raw_text.split()
