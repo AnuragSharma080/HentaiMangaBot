694c89a0820ce710d021c046f1c148c9893fc3ab
P4IN SENPAI <86664546+Pain-Senpai@users.noreply.github.com>
2021-12-24 17:48:23 +0530

    Update starter.py

--- Plugins/starter.py
+++ Plugins/starter.py
@@ -9,7 +9,7 @@ class start():
         await bot.send_message(
             event.chat_id,
             start_text,
-            file='https://telegra.ph/file/0825336417b74a6c10fd7.mp4'
+            file='https://telegra.ph/file/1cf0b127f3c888b1d1b21.mp4'
         )
 
     @bot.on(events.NewMessage(pattern="/help"))
@@ -17,12 +17,12 @@ class start():
         await bot.send_message(
             event.chat_id,
             help_text
-       )
+            )
 
     @bot.on(events.NewMessage(pattern="/source"))
     async def event_handler_source(event):
         await bot.send_message(
             event.chat_id,
-            '[Network](https://t.me/Project_Tsukiyomi/6)\nProvides you some doujins'
+            '[Bot Dev](https://t.me/Yeageristbotsdev)\nThis bot was hosted on Heroku'
         )
     
