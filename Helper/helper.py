8a5db4397dbdeb19c83a7eb03ddbcbddcf6e072d
P4IN SENPAI <86664546+Pain-Senpai@users.noreply.github.com>
2021-12-24 17:18:00 +0530

    Update helper.py

--- Helper/helper.py
+++ Helper/helper.py
@@ -1,12 +1,12 @@
-start_text = '''Welcome to Hentai Manga DL Bot. Here you can Download all Hentai Mangas for Free.
+start_text = '''OwO User Kun! Thanks For Starting me. Here you can Download all Hentai Mangas for Free.
 
 For many who don’t know about Hentai, this is about Japanese anime and manga porn or cartoon porn. 
 This great genre was born in Japan and its real meaning comes from perverse
 sexual desire or perverse sexual act since in the vast majority of porn manga we will find fabulous stories of forbidden and very erotic sex.
 
-Note : This Bot Is only for adults
+Note : This Bot Is only for Horny Adults
 
-If Any Abuse About This Bot you can report @Pain_to_this_world
+If you have any query, wanna contribute, donate come to @AnimeTsukiyomi
 
 Use /help to know commands and how to use this bot'''
 
@@ -16,7 +16,7 @@ help_text = '''List of commands:
                                
 The files provided are in multiple qualities to download just open file in chrome
                                
-To report any Problems, Bugs, Suggestions go to @splits_support
+To report any Problems, Bugs, Suggestions go to @AnimeTsukiyomi
 
-Note : This bot is only for adult'''
+Note : This bot is only for Certified Honrnies'''
 
